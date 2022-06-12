6c6ea8262c743b00883bb4d27a36d0ac0744ec8a
jean paul <jeanpaulabizeid@gmail.com>
2022-06-12 11:35:36 +0300

    registering user according to the response

--- frontend-user/pages/sign-up/scripts/singup.js
+++ frontend-user/pages/sign-up/scripts/singup.js
@@ -32,3 +32,35 @@ function reveal() {
   }
 }
 
+
+let signUp= (e) =>{
+    e.preventDefault();
+    let data = new FormData();
+  
+    data.append('first_name', document.getElementById("first_name").value);
+    data.append('last_name', document.getElementById("last_name").value);
+    data.append('email', document.getElementById("email").value);
+    data.append('password', document.getElementById("password").value);
+  
+    //linking with sign up api
+    axios({
+      method: 'post',
+      url: 'http://127.0.0.1:8000/api/register',
+      data: data,
+    })
+    .then(function (response) {
+        if(response.data.message == "error"){
+            alert("email already used or incorrect format")
+        }
+        
+        var token = response.data.authorisation.token;
+        localStorage.setItem("jwt", token);
+        window.location.href = "../explore/explore.html";
+    })
+    .catch(function (error){
+      console.log(error);
+    })
+}
+
+sign_up_btn.addEventListener('click', signUp)
+
