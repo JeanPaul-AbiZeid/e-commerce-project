b088d462d9740e4590b298f4a208301b7d76340c
jean paul <jeanpaulabizeid@gmail.com>
2022-06-12 09:48:13 +0300

    creating items according to the database

--- frontend-user/scripts/landing.js
+++ frontend-user/scripts/landing.js
@@ -16,3 +16,49 @@ look_at.addEventListener("click", function(){
 log_in.addEventListener("click", function(){
     window.location.href = "./pages/log-in/login.html";
 })
+
+axios({
+    url: 'http://127.0.0.1:8000/api/getitems',
+}).then(function(response){
+    // console.log(response.data.items[0].name); 
+    //looping over the array to get items data
+    for(let i=0; i<response.data.items.length; i++){
+        let name = response.data.items[i].name;
+        let description = response.data.items[i].description;
+        let category = response.data.items[i].category;
+        createItem(name, description, category);
+    }
+    
+    // <div class="product-card">
+    //     <div class="product-image"><img src="./assets/product.jpg"></div>
+    //     <h3>Name</h3>
+    //     <p>description</p>
+    // </div>
+})
+
+function createItem(name, description, category){
+    const main = document.createElement("div");
+    main.className = "product-card";
+    const container = document.querySelector(".product-container");
+    container.appendChild(main);
+
+    const image_container = document.createElement("div");
+    image_container.className = "product-image";
+    const image = document.createElement("img");
+    image.src = "./assets/product.jpg";
+
+    image_container.appendChild(image);
+    main.appendChild(image_container);
+
+    const product_name = document.createElement("h3");
+    product_name.innerText = name;
+    main.appendChild(product_name);
+
+    const product_category = document.createElement("h4");
+    product_category.innerText = "Category: " + category;
+    main.appendChild(product_category);
+
+    const product_desc = document.createElement("p");
+    product_desc.innerText = description;
+    main.appendChild(product_desc);
+}
