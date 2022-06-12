a377eef6264f50ad8dca5de6cda29e33cce97122
jean paul <jeanpaulabizeid@gmail.com>
2022-06-12 15:09:27 +0300

    creating the admin page + styling + fixing the log in system

--- electron-admin/scripts/login.js
+++ electron-admin/scripts/login.js
@@ -11,3 +11,37 @@ function reveal() {
     x.type = "password";
   }
 }
+
+const log_in_btn = document.getElementById("login");
+
+ //function for log in button on click
+let logIn = (e)=>{
+    e.preventDefault();
+    let data = new FormData();
+  
+    data.append('email', document.getElementById("email").value);
+    data.append('password', document.getElementById("password").value);
+  
+    //linking with login api
+    axios({
+      method: 'post',
+      url: 'http://127.0.0.1:8000/api/login',
+      data: data,
+    })
+    .then(function (response) {
+        // var token = response.data.authorisation.token
+        // localStorage.setItem("jwt", token);
+        // window.location.href = "../explore/explore.html";
+        console.log(response)
+    })
+    .catch(function (error){
+        // if(error.response.data.message == "Unauthorized"){
+        //     alert("Incorrect email or password");
+        // }
+        // else{
+        //     alert(error.response.data.message);
+        // }
+    })
+  }
+
+  log_in_btn.addEventListener('click', logIn);
