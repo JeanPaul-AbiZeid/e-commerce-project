9a5ae78f86360463f649a69efe300600676b7722
jean paul <jeanpaulabizeid@gmail.com>
2022-06-13 08:32:27 +0300

    linking the logout button

--- electron-admin/scripts/admin.js
+++ electron-admin/scripts/admin.js
@@ -25,3 +25,10 @@ if (event.target == modal) {
     modal.style.display = "none";
 }
 }
+
+const log_out_btn = document.getElementById("logout");
+
+log_out_btn.addEventListener("click", function(){
+    localStorage.removeItem("jwt");
+    window.location.href = "./index.html";
+})
