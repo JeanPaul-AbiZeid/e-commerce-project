7bfa969108734d0bb8ef163c4a1515a7b2bf4359
jean paul <jeanpaulabizeid@gmail.com>
2022-06-13 21:52:00 +0300

    adding the add item feature

--- electron-admin/scripts/admin.js
+++ electron-admin/scripts/admin.js
@@ -1,30 +1,30 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+// // Get the modal
+// var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("add-item");
+// // Get the button that opens the modal
+// var btn = document.getElementById("add-item");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close");
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close");
 
-//checkbox for showing password
-var show = document.getElementById("toggle");
+// //checkbox for showing password
+// var show = document.getElementById("toggle");
 
 
-// When the user clicks the button, open the modal 
-btn.addEventListener("click", function(){
-    modal.style.display = "block";})
+// // When the user clicks the button, open the modal 
+// btn.addEventListener("click", function(){
+//     modal.style.display = "block";})
 
-// When the user clicks on <span> (x), close the modal
-span[0].addEventListener("click", function(){
-    modal.style.display = "none";})
+// // When the user clicks on <span> (x), close the modal
+// span[0].addEventListener("click", function(){
+//     modal.style.display = "none";})
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-if (event.target == modal) {
-    modal.style.display = "none";
-}
-}
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+// if (event.target == modal) {
+//     modal.style.display = "none";
+// }
+// }
 
 const log_out_btn = document.getElementById("logout");
 const add_category = document.getElementById("add-category");
@@ -102,8 +102,7 @@ function createItem(name, description, category){
 
 function createCategory(id, name){
     const cat = document.createElement("option");
-    cat.id = id;
-    cat.value = name;
+    cat.value = id;
     cat.innerHTML = name;
 
     const drop = document.getElementById("categories")
@@ -121,22 +120,34 @@ axios({
     }
 })
 
-const categ = document.getElementsByClassName("category")
-const cat_button = document.querySelector(".dropbtn")
+const add_item = document.getElementById("add-item");
+function getInput(){
+    jwt = localStorage.getItem("jwt")
 
-// categ.forEach(function(item){
-//     item.addEventListener("click",function(){
-//         console.log("test")
-//     })
-// })
-console.log(categ.length)
-console.log("hi")
-// for (var j = 0; j < categ.length; j++) {
-//     categ[j].addEventListener("click",function(){
-//     console.log("test");
-// })
-// }
+    let data = new FormData();
+    
+    data.append('name', document.getElementById("item-name").value);
+    data.append('description', document.getElementById("item-description").value);
+    let select_item = document.getElementById("categories");
+    data.append('category_id', select_item.options[select_item.selectedIndex].value);
 
+    axios({
+        method : 'post',
+        url : 'http://127.0.0.1:8000/api/additem',
+        headers: {
+            'Authorization': `token ${jwt}`
+        },
+        data: data
+    })
+    .then((response) => {
+        console.log(response)
+    })
+    .catch((error) => {
+    console.error(error)
+    })
+}
+
+add_item.addEventListener('click', getInput)
 
 
 
